2ce54a09231d74d9ed291d99c206a799c67fc749
ysy2025 <ysy2025.outlook.com>
2022-02-09 19:50:47 +0800

    day4.2

--- myJava01/src/main/java/com/ysy/day04/Cycle01.java
+++ myJava01/src/main/java/com/ysy/day04/Cycle01.java
@@ -8,11 +8,36 @@ import java.util.Scanner;
 顺序
 
 分支
+
 if-else 条件判断
 if
 if-else
 if-else if-else
+
 switch-case
+switch (表达式1){
+case 常量1:
+    执行语句1;
+case 常量2:
+    执行语句2;
+case 常量3:
+    执行语句3;
+...
+default:
+    执行语句n;
+    break;
+    }
+switch找到匹配的case,调用语句
+调用完语句后,仍然继续执行后续语句,直到遇到关键字break或者结构末尾才结束
+
+想实现多选一,在case执行语句中加break
+
+switch结构中的表达式只能是以下6种数据类型:byte short char int 枚举 string
+case之后只能声明常量
+break关键字不加也没问题
+default,相当于if-else中的else,备胎;可选的,位置灵活.
+
+分支结构可以用switch-case且ifelse,且取值不多,优先switch-case
 循环
  */
 public class Cycle01 {
@@ -40,6 +65,17 @@ public class Cycle01 {
             System.out.println("Pretty");
         }else {
             System.out.println("Bad!");
+        }
+
+        int aa = 100;
+        switch(aa){
+            case 10:
+                System.out.println("aa niubi!");
+            case 100:
+                System.out.println("aa !!!!!");
+            default:
+                System.out.println("~~~~~~");
+            break;
         }
     }
 }
---
+++ myJava01/src/main/java/com/ysy/day04/While01.java
@@ -0,0 +1,32 @@
+package com.ysy.day04;
+
+import java.util.Scanner;
+
+/*
+while 循环
+
+初始化条件
+while(循环条件){
+循环体
+迭代条件
+}
+
+while循环注意迭代条件
+避免死循环
+
+for循环和while循环的初始化条件作用域不同.
+ */
+public class While01 {
+    public static void main(String[] args) {
+        System.out.println("zhangsan");
+
+        Scanner sc = new Scanner(System.in);
+
+        int a = sc.nextInt();
+        while(a>0){
+            System.out.println(a);
+            a--;
+        }
+    }
+
+}
