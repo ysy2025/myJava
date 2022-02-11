be4a4f3237ba99f5772b12afa91c42d398828dd3
ysy2025 <ysy2025.outlook.com>
2022-02-11 19:55:42 +0800

    day05-array

--- myJava01/src/main/java/com/ysy/day04/Cycle03.java
+++ myJava01/src/main/java/com/ysy/day04/Cycle03.java
@@ -4,11 +4,24 @@ package com.ysy.day04;
 嵌套循环
 内外层循环
 
-
+break
+continue
+带label的
  */
 
 public class Cycle03 {
     public static void main(String[] args) {
         System.out.println("zhangsan");
+
+        label: for(int i = 1; i < 4; i++){
+            for (int j = 1; j < 10; j++){
+                if(j % 4 == 0){
+//                    break;
+                    continue label;
+                }
+                System.out.print(j);
+            }
+            System.out.println();
+        }
     }
 }
---
+++ myJava01/src/main/java/com/ysy/day05/Array02.java
@@ -0,0 +1,22 @@
+package com.ysy.day05;
+
+import com.ysy.day04.Utility01;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+/*
+一维数组的使用
+1,声明和初始化
+2,如何调用数组指定位置元素
+3,如何获取数组长度
+4,遍历数组
+5,数组元素默认初始化值
+6,数组内存解析
+ */
+public class Array02 {
+    ArrayList<String> aa1 = new ArrayList<String>();
+    Scanner a0 = new Scanner(System.in);
+    String a1 = Utility01.readKeyBoard(10);
+
+}
