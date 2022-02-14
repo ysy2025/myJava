b26f50afbc1cc3874741613ef32c79a8706100e2
ysy2025 <ysy2025@outlook.com>
2022-02-14 23:56:23 +0800

    day07-OOP

--- myJava01/src/main/java/com/ysy/day06/Sort02.java
+++ myJava01/src/main/java/com/ysy/day06/Sort02.java
@@ -1,6 +1,6 @@
 package com.ysy.day06;
 
-public class Sort2 {
+public class Sort02 {
     public static void main(String[] args) {
 
     }
---
+++ myJava01/src/main/java/com/ysy/day07/klass01.java
@@ -0,0 +1,33 @@
+package com.ysy.day07;
+
+/*
+面向对象
+1,类和类成员
+
+属性
+方法
+构造器
+
+代码块
+内部类
+
+2,面向对象的三大特征
+封装性
+继承性
+多态性
+
+3,其他关键字
+this
+super
+final
+abstract
+interface
+package
+public private
+static
+void
+
+ */
+public class klass01 {
+    
+}
