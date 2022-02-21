0961b719f4a9152694a2161b1bf05e98f3233ecc
ysy2025 <ysy2025.outlook.com>
2022-02-21 19:57:13 +0800

    大衣-1.0

---
+++ myJava01/src/main/java/com/ysy/day09/oop04.java
@@ -0,0 +1,22 @@
+package com.ysy.day09;
+
+/*
+属性赋值的先后顺序
+
+属性可以在哪赋值?
+1,默认初始化
+2,显式初始化
+3,构造器
+4,方法(setter)(对象.方法或者对象.属性)
+
+ */
+public class oop04 {
+
+}
+
+class Book{
+    int id;
+    int price;
+    int pages;
+
+}
