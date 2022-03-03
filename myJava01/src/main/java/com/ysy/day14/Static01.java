36522c5fdd1ca47f3739a8c1f760e11f2633b820
ysy2025 <ysy2025@outlook.com>
2022-03-03 22:58:04 +0800

    day14-2.0

--- myJava01/src/main/java/com/ysy/day14/Static01.java
+++ myJava01/src/main/java/com/ysy/day14/Static01.java
@@ -11,10 +11,33 @@ static 静态的
     静态变量:我们创建了类的多个对象,每个对象共享非静态属性.修改其中一个对象的非静态属性时,会导致其他对象中同样属性值修改
 
 static 修饰属性的其他说明
-    随着类的加载而加载
+    随着类的加载而加载,可以通过类.静态变量进行调用
+    静态变量的加载早于对象创建
+    由于类只会加载一次,因此静态变量在内存中只会存一份
 
-static 修饰方法
+静态属性案例:System.out, Math.PI
+
+
+static 修饰方法:静态方法
+    静态方法,随着类的加载而加载;可以直接用 类.方法 来调用
+    静态方法,之所以需要确保调用的field都是static的,就是为了确保能够随着类加载而加载,而不是随着instance而加载.
+    非静态方法,可以调用非静态属性或静态属性,或方法.
+    这是由生命周期确定的.
+
+static 静态结构中,不能使用this关键字,super也不能使用,因为这两个都是需要先有对象,再调用
+
  */
 public class Static01 {
 
+}
+
+
+class Chinese{
+    int age;
+    int id;
+    String name;
+
+//    public static void show(){
+//        System.out.println("I am " + name + " and I am " + age + " years old ages!");
+//    }
 }
