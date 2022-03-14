6d6498658c410b5ae316f4c7e3aa36618805a7d4
ysy2025 <ysy2025@outlook.com>
2022-03-14 23:06:21 +0800

    day16-1.0

---
+++ myJava01/src/main/java/com/design/day01/principle/singleresponsibility/SingleResponbility3.java
@@ -0,0 +1,30 @@
+package com.design.day01.principle.singleresponsibility;
+
+import com.ysy.day11.NewPencil;
+
+/*
+* 方法3,对于原来的类没有大修改,只要增加方法
+* 方法级别上符合单一职责原则,在类级别没有遵守单一职责
+ */
+public class SingleResponbility3 {
+    public static void main(String[] args) {
+        NeWVehicle neWVehicle = new NeWVehicle();
+
+        neWVehicle.run("car");
+
+        neWVehicle.run("plane");
+    }
+}
+
+class NeWVehicle{
+    public void run(String vehicle){
+        System.out.println(vehicle + " is running on the road.");
+    }
+
+    public void runAir(String vehicle){
+        System.out.println(vehicle + " is running on the air.");
+    }
+    public void runWater(String vehicle) {
+        System.out.println(vehicle + " is running on the water.");
+    }
+}
