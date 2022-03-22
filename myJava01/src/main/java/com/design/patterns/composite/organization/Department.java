eb05e8d728145a42561540c7a3162a6375ad340c
ysy2025 <ysy2025@outlook.com>
2022-03-22 23:04:57 +0800

    day17-1.3

---
+++ myJava01/src/main/java/com/design/patterns/composite/organization/Department.java
@@ -0,0 +1,28 @@
+package com.design.patterns.composite.organization;
+
+// department 是最小单位,没办法细分了
+public class Department extends Organization {
+
+    // 构造器
+    public Department(String name, String desc) {
+        super(name, desc);
+    }
+
+    @Override
+    public void show() {
+        System.out.println("This is " + getName() + " and it is " + getDesc());
+    }
+
+    //add,remove这些就不需要了,叶子节点,不需要重写
+
+
+    @Override
+    public String getName() {
+        return super.getName();
+    }
+
+    @Override
+    public String getDesc() {
+        return super.getDesc();
+    }
+}
---
+++ myJava01/src/main/java/com/design/patterns/composite/organization/Organization.java
@@ -0,0 +1,39 @@
+package com.design.patterns.composite.organization;
+
+public abstract class Organization {
+
+    public String name;
+
+    public String desc;
+
+    public void add(Organization org){
+        throw new UnsupportedOperationException();
+    };
+
+    public void remove(Organization org){
+        throw new UnsupportedOperationException();
+    };
+
+    public Organization(String name, String desc) {
+        this.name = name;
+        this.desc = desc;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDesc() {
+        return desc;
+    }
+
+    public void setDesc(String desc) {
+        this.desc = desc;
+    }
+
+    public abstract void show();
+}
