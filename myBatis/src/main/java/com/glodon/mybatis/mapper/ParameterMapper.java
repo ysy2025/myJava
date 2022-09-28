985e9bca42ad15041fa13c0efa7ae3cfd225ccd4
ysy2025 <ysy2025@outlook.com>
2022-09-28 23:13:29 +0800

    mybatis 1.2

--- myBatis/src/main/java/com/glodon/mybatis/mapper/ParameterMapper.java
+++ myBatis/src/main/java/com/glodon/mybatis/mapper/ParameterMapper.java
@@ -3,6 +3,7 @@ package com.glodon.mybatis.mapper;
 import com.glodon.mybatis.pojo.Fifa;
 
 import java.util.List;
+import java.util.Map;
 
 public interface ParameterMapper {
 
@@ -16,4 +17,16 @@ public interface ParameterMapper {
      * 根据用户名查询用户信息
      */
     Fifa getFifaByName(String name);
+
+    /**
+     * 验证登录
+     */
+    Fifa checkLogin(String name, int age);
+
+    Fifa checkLoginByMap(Map<Object, Object> map);
+
+    /**
+     * 查看信息
+     */
+    Fifa selectDetail(Fifa fifa);
 }
--- myBatis/src/test/java/com/glodon/mybatis/test/myBatisTest2.java
+++ myBatis/src/test/java/com/glodon/mybatis/test/myBatisTest2.java
@@ -10,7 +10,9 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class myBatisTest2 {
 
@@ -56,4 +58,38 @@ public class myBatisTest2 {
         System.out.println(fifa);
     }
 
+    @Test
+    public void testCheckLogin(){
+        /**2,mapper接口方法的参数为多个参数时,如何?
+         * 会放到map中;map中如何存储数据?1,arg0,arg1为键,参数为值;2,param1,param2为键,参数为值
+         * 可不可以手动将参数放到map中,然后将map集合作为mapper接口的参数传入?自定义map
+         *
+         *
+         */
+        SqlSession sqlSession = SqlSessionUtils.getSqlSession();
+        ParameterMapper mapper = sqlSession.getMapper(ParameterMapper.class);
+        Fifa fifa = mapper.checkLogin("zhangsan", 31);
+        System.out.println(fifa);
+    }
+
+    @Test
+    public void testCheckLoginByMap(){
+        /**
+         * 如果mapper的接口方法参数有多个,可以手动将参数放在一个map中存储;
+         */
+        SqlSession sqlSession = SqlSessionUtils.getSqlSession();
+        ParameterMapper mapper = sqlSession.getMapper(ParameterMapper.class);
+        Map<Object, Object> map = new HashMap<Object, Object>();
+        map.put("id", 11);
+        map.put("season", 1);
+        Fifa fifa = mapper.checkLoginByMap(map);
+        System.out.println(fifa);
+    }
+
+    @Test
+    public void test1(){
+        /**4.mapper接口的参数是一个实体类类型的参数的时候,以属性名来访问
+         * 只需要通过#{}和${}以属性的方式访问属性值即可但是需要注意${}的单引号问题
+         */
+    }
 }
