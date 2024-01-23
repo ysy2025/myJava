1da00b71e54682ab9dfbb1178bad00bb1b93997a
unknown <ysy2025@outlook.com>
2024-01-23 22:48:07 +0800

    myKafka

--- myKafka/src/main/java/org/ysy/kafka/producer/myProducer.java
+++ myKafka/src/main/java/org/ysy/kafka/producer/myProducer.java
@@ -14,22 +14,22 @@ public class myProducer {
         Properties properties = new Properties();
 
         // 0.1 让配置与虚拟机的kafka产生关系
-        properties.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG,"192.168.10.102:9092,192.168.10.103:9092");
+        properties.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG,"192.168.10.104:6667");
 
-        System.out.println("========================11111111111111============================");
+        properties.put("retries", 1);
+
+        properties.put("linger.ms", 1);
+
         // 0.2 指定对应key和value的序列化类
         properties.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
 
         properties.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
-        System.out.println("========================222222222222222============================");
 
         // 1,创建kafka生产者对象
         KafkaProducer<String, String> myKafkaProducer = new KafkaProducer<String, String>(properties);
-        System.out.println("========================333333333333333============================");
 
         // 2.发送数据
         myKafkaProducer.send(new ProducerRecord<>("first", "zhangsan"));
-        System.out.println("========================4444444444444444============================");
         for (int i = 0; i < 5; i++) {
             myKafkaProducer.send(new ProducerRecord<>("first", "zhangsan" + i));
             System.out.println("========================hhhhhhhhhhhhhh============================");
